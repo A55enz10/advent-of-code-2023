0668ba1193fe7bfe9200df6b3db3fe761182d159
niccolo.bergamini <niccolo.bergamini@qooa.it>
2023-12-01 11:28:36 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,38 @@
 fun main() {
+
     fun part1(input: List<String>): Int {
-        return input.size
+
+        return input.map { str -> str.filter { it.isDigit() } }
+            .map { it.substring(0, 1) + it.substring(it.length - 1) }
+            .sumOf { it.toInt() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map { it.getDigits() }
+            .map { it.substring(0, 1) + it.substring(it.length - 1) }
+            .sumOf { it.toInt() }
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+}
+
+private fun String.getDigits(): String {
+    val digitList =  arrayOf("zero","one","two","three","four","five","six","seven","eight","nine")
+    var digitStr = ""
+    var i = 0
+    while (i < this.length) {
+        val subStr = this.substring(i)
+        if (subStr.first().isDigit())
+            digitStr += subStr.first()
+        else
+            digitList.forEach{
+                if (subStr.startsWith(it)) {
+                    digitStr += digitList.indexOf(it)
+                }
+            }
+        i++
+    }
+    return digitStr
 }
